0459c220ea6c148424ae6a44565eba35b655764f
KingVishal703 <vishal21wankhede@gmail.com>
2025-06-24 22:31:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from config import API_ID, API_HASH, BOT_TOKEN
 import getvideo
 import start
 import verify
+import broadcast
 
 app = Client("getvideobot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
