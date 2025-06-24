46a140db603f32af4a5440cbbb5151662a54767b
KingVishal703 <vishal21wankhede@gmail.com>
2025-06-24 22:47:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ API_ID = 123456       # apna Telegram API ID
 API_HASH = "abcxyz"   # apna Telegram API HASH
 ADMINS = [5654093580]  # apna Telegram user ID daalo (int), multiple bhi daal sakte ho
 
-BOT_TOKEN = "7614452604:AAEUpLVH3uh7RgKJnZOuOHLuZv-YMzG_Q2g"
+BOT_TOKEN = ""
 
-MONGO_URL = "mongodb://username:password@host:port/dbname"
+MONGO_URL = ""
 
-CHANNEL_ID = -1001234567890  # jahan se video fetch karna hai
-FORCE_SUB_CHANNEL = "yourchannelusername"  # without @
+CHANNEL_ID = -  # jahan se video fetch karna hai
+FORCE_SUB_CHANNEL = ""  # without @
